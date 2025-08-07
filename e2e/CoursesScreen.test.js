2641c23d849f38618b6dc2b9358c1f05499eb095
Роман Круглянский <romankruglanskij@Noutbuk-Roman.local>
2025-08-07 14:32:06 +0300

    add android jest ci 27

--- e2e/CoursesScreen.test.js
+++ e2e/CoursesScreen.test.js
@@ -1,31 +1,43 @@
-import {device, element, by, expect} from 'detox';
+import { device, element, by, expect, waitFor } from 'detox';
 
 describe('Courses Screen E2E', () => {
   beforeAll(async () => {
-    await device.launchApp({newInstance: true});
+    await device.launchApp({ newInstance: true });
   });
 
   it('показывает все курсы по умолчанию', async () => {
-    await expect(element(by.id('course-card-course:1'))).toBeVisible(50);
-    // await expect(element(by.id('course-card-course:2'))).toBeVisible(50);
-    await expect(element(by.id('course-card-course:3'))).not.toBeVisible(50);
+    await waitFor(element(by.id('course-card-course:1'))).toBeVisible().withTimeout(10000);
+    await expect(element(by.id('course-card-course:1'))).toBeVisible();
+    // await expect(element(by.id('course-card-course:2'))).toBeVisible();
+    await expect(element(by.id('course-card-course:3'))).not.toBeVisible();
   });
 
   it('открывает модалку и фильтрует курсы по тегу "Головоломки"', async () => {
+    await waitFor(element(by.id('selector-button'))).toBeVisible().withTimeout(10000);
+    await waitFor(element(by.id('selector-button'))).toBeTouchable().withTimeout(10000);
     await element(by.id('selector-button')).tap();
-    waitFor(element(by.id('selector-modal'))).toBeVisible();
 
+    await waitFor(element(by.id('selector-modal'))).toBeVisible().withTimeout(10000);
+
+    await waitFor(element(by.id('selector-modal-tag-Головоломки'))).toBeVisible().withTimeout(10000);
     await element(by.id('selector-modal-tag-Головоломки')).tap();
 
-    await expect(element(by.id('course-card-course:1'))).toBeVisible(50);
-    // await expect(element(by.id('course-card-course:5'))).toBeVisible(50);
-    await expect(element(by.id('course-card-course:3'))).not.toBeVisible(50);
+    await waitFor(element(by.id('course-card-course:1'))).toBeVisible().withTimeout(10000);
+    await expect(element(by.id('course-card-course:1'))).toBeVisible();
+    // await expect(element(by.id('course-card-course:5'))).toBeVisible();
+    await expect(element(by.id('course-card-course:3'))).not.toBeVisible();
   });
 
   it('закрывает модалку', async () => {
+    await waitFor(element(by.id('selector-button'))).toBeVisible().withTimeout(10000);
+    await waitFor(element(by.id('selector-button'))).toBeTouchable().withTimeout(10000);
     await element(by.id('selector-button')).tap();
-    waitFor(element(by.id('selector-modal'))).toBeVisible();
+
+    await waitFor(element(by.id('selector-modal'))).toBeVisible().withTimeout(10000);
+    await waitFor(element(by.id('selector-modal-close-button'))).toBeVisible().withTimeout(10000);
     await element(by.id('selector-modal-close-button')).tap();
+
+    await waitFor(element(by.id('selector-modal'))).not.toBeVisible().withTimeout(10000);
     await expect(element(by.id('selector-modal'))).not.toBeVisible();
   });
 });
